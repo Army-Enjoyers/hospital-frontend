2ae3575e33eee662b1c8233a5d3005639420c294
4llower <95350098@study.bsuir.by>
2021-04-16 03:24:01 +0300

    final fix: form generator

--- src/modules/Home/components/References/References.tsx
+++ src/modules/Home/components/References/References.tsx
@@ -5,29 +5,23 @@ import { createClient } from '~/api'
 import { Input } from '~/components'
 import { JSONSchemaFormField } from '~/types'
 
-import { useJSONSchema } from './hooks'
-
 export const References: React.FC = () => {
   const [schema, setSchema] = useState<JSONSchemaFormField[] | null>(null)
+  const [values, setValues] = useState<Record<string, string>>()
 
   useEffect(() => {
     const client = createClient()
-    void client.getFromJSONSchema().then((data) => setSchema(data))
+    void client.getFromJSONSchema().then((data) => {
+      setSchema(data)
+    })
   }, [setSchema])
 
-  if (!schema) {
-    return (
-      <Flex alignItems="center" height="50vh" justifyContent="center" width="60%">
-        <Spinner height="100px" width="100px" />
-      </Flex>
-    )
-  }
+  useEffect(() => {
+    if (schema)
+      setValues(schema.map((elem) => ({ [elem.id]: '' })).reduce((r, c) => Object.assign(r, c)))
+  }, [schema])
 
-  const [values, setValues] = useState(
-    schema.map((elem) => ({ [elem.id]: '' })).reduce((r, c) => Object.assign(r, c)),
-  )
-
-  const onSubmit = (values: any) => {
+  const onSubmit = () => {
     console.log(values)
   }
 
@@ -37,6 +31,14 @@ export const References: React.FC = () => {
     },
     [setValues],
   )
+
+  if (!schema || !values) {
+    return (
+      <Flex alignItems="center" height="50vh" justifyContent="center" width="60%">
+        <Spinner height="100px" width="100px" />
+      </Flex>
+    )
+  }
 
   return (
     <Box>
